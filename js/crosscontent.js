c3f3a4266431fea991611281a959e9e4c1e91794
Vignesh Subbu <vigneshsubbu9990@users.noreply.github.com>
2017-06-19 11:35:02 -0400

    Dropdown Action
    Correcting overall content statistics dropdown action
    Adding styles for crosscontent page
    Dynamic Series Data update for the Cross Content Chart

--- js/crosscontent.js
+++ js/crosscontent.js
@@ -1,8 +1,10 @@
 
 var Chart = (function () {
+	var contentData, crossContentStatisticsChart, compareType="volume";
  
         function setCrossContentStatisticsChart(data) {
-		        var crossContentStatisticsChart = Highcharts.chart('crosscontent_container', {
+        		contentData = data;
+		        crossContentStatisticsChart = Highcharts.chart('crosscontent_container', {
 			        chart: {
 			            plotBackgroundColor: null,
 			            plotBorderWidth: null,
@@ -28,73 +30,89 @@ var Chart = (function () {
 			        series: [{
 			            name: 'Substance Coverage',
 			            colorByPoint: true,
-			            data: [{
-			                name: data.content.chemadvisor.name,
-			                y: data.content.chemadvisor.volume
-			            }, {
-			                name: data.content.synapse.name,
-			                y: data.content.synapse.volume,
-			                sliced: true,
-			                selected: true
-			            }, {
-			                name: data.content.actio.name,
-			                y: data.content.actio.volume
-			            },{
-			                name: data.content.reaxys.name,
-			                y: data.content.reaxys.volume
-			            }]
+			            data: [
+        						{	name: contentData.content.chemadvisor.name,
+					                y: getContentNumbers(contentData.content.chemadvisor)
+					            }, 
+					            {
+					                name: contentData.content.synapse.name,
+					                y: getContentNumbers(contentData.content.synapse),
+					                sliced: true,
+					                selected: true
+					            }, 
+					            {
+					                name: contentData.content.actio.name,
+					                y: getContentNumbers(contentData.content.actio)
+					            },
+					            {
+					                name: contentData.content.reaxys.name,
+					                y: getContentNumbers(contentData.content.reaxys)
+					            }]
 			        }]
-
 			    });
 
+        }
+
+        function getSeriesData() {
+        	var seriesData = [
+        						{	name: contentData.content.chemadvisor.name,
+					                y: getContentNumbers(contentData.content.chemadvisor)
+					            }, 
+					            {
+					                name: contentData.content.synapse.name,
+					                y: getContentNumbers(contentData.content.synapse),
+					                sliced: true,
+					                selected: true
+					            }, 
+					            {
+					                name: contentData.content.actio.name,
+					                y: getContentNumbers(contentData.content.actio)
+					            },
+					            {
+					                name: contentData.content.reaxys.name,
+					                y: getContentNumbers(contentData.content.reaxys)
+					            }];
+        	return seriesData;
         }
  
         function getCrossContent() {
             $.getJSON( "resources/content-statistics.json", setCrossContentStatisticsChart);
         }
  
-        function setChart(series) {
+        function setChart(compareType) {
         	getCrossContent(); 
         }
+
+        function resetChartData(){
+        	crossContentStatisticsChart.series[0].setData(getSeriesData());
+        }
   
+        function getContentNumbers(contentVendor) {
+        	return (compareType == "volume") ? contentVendor.volume : contentVendor.identifiers[compareType].total;
+
+        }
+
+        function setCompareType(compareTypeReference){
+        	compareType = compareTypeReference;
+        }
+
+        function getCompareType(){
+        	return compareType;
+        }
+
         return {
-            setChart: setChart
+            setChart: setChart,
+            getSeriesData: getSeriesData,
+            resetChartData: resetChartData,
+            setCompareType: setCompareType
         };
  
     })();
- 
-
-// $("#preselect-materials").on("click", function(){
-
-// });
-
-// $("#prime-search").on("keyup", function(){
-// 	var searchKey = $(this).val();
-
-// 	if(!searchKey || searchKey.length < 3) {
-// 		$("#main-container").hide("slow");
-// 		$("#sub-container").hide("slow");
-// 		return;
-// 	}
-
-// 	$("#main-container").hide("slow");
-// 	$("#sub-container").show("slow");
-// 	$("#sub-container .sub-list-group").empty();
-// 	if( $( "#sub-container" ).has( "h3" ) && !$( "#sub-container h3").text().length ) {
-// 		$("#sub-container").prepend("<h3>Material Safety Data Sheets</h3>");
-// 	}
-	
-// 	// if(searchKey.length < 2) return;
-// 	$( ".main-list-group a:contains('"+searchKey+"')" ).clone().appendTo($("#sub-container .sub-list-group"));
-
-// 	if($("#sub-container .sub-list-group a").length == 0)
-// 	$("#sub-container h3").text("");
-// });
 
 $(".dropdown > ul > li > a").on("click", function(){
-    var $a = $(this);
-    alert($a.html());
-    $("#chartInput").text($a.html());
+	Chart.setCompareType($(this).data("value"));
+    Chart.resetChartData();
+    $("#chartInput").text($(this).html());
     $(".dropdown").removeClass("open");
     return false;
 });
